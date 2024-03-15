4a411a1309bd8e4e9a8572684fd75b3d3c580ab0
prajwalhc-18 <prajwalhc1806@gmail.com>
2024-03-15 20:34:28 +0530

    toggle text

---
+++ Toggle Text/script.js
@@ -0,0 +1,7 @@
+// script.js
+$(document).ready(function() {
+    // jQuery code will go here
+    $('#toggleButton').click(function() {
+        $('#toggleParagraph').toggle(); // Toggle the visibility of the paragraph
+    });
+});
